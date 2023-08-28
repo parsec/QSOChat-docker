52ce8b059a58cef87d52eb2ea9c4134370f775ba
parsec <beckmanseth@gmail.com>
2023-08-28 11:59:07 -0400

    fix: vars

---
+++ fly/applications/qsochat/ChatServer/customvars.js
@@ -0,0 +1,16 @@
+module.exports.hostname = ''; // change to the hostname or IP of your host
+module.exports.port = 80;
+module.exports.hamqthcall = 'n4sbx';
+module.exports.hamqthapipw = 'enx*yma0bnm!ENE!nyp';
+module.exports.hamqthprog = 'kk4gq-qsochat';
+module.exports.title = 'KK4GQ QSOchat'; //change to your title for page
+module.exports.logo = '/images/d-star-logo.png'; //change to your logo in images directory
+module.exports.links = [
+        {href:'http://www.youtube.com/watch?v=oGF-qkdoid4',description:'HF D-STAR Video'},
+        {href:'https://github.com/g4klx',description:'G4KLX Software'},
+        {href:'https://github.com/johnhays/QSOChat',description:'QSOChat Source'},
+        {href:'http://www.dstarinfo.com/DSTARHFNet.aspx',description:'HF Net Schedule'},
+        {href:'https://nwdigitalradio.com/',description:'NW Digital Radio'}
+]; //a JSON encoded list of links
+module.exports.standardfreqs = [ "7.287.5", "5346.5", "3.975" ]; //an array of frequencies to use for preselect buttons;
+

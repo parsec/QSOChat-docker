4e60ca35f3aab654986a93f84151e7b7f707ef08
parsec <beckmanseth@gmail.com>
2023-08-30 09:52:35 -0400

    fix: links

--- fly/applications/qsochat/ChatServer/customvars.js
+++ fly/applications/qsochat/ChatServer/customvars.js
@@ -7,8 +7,7 @@ module.exports.title = 'KK4GQ QSOchat'; //change to your title for page
 module.exports.logo = '/images/kk4gq.png'; //change to your logo in images directory
 module.exports.links = [
         {href:'https://kk4gq.org',description:'Fayette County Amateur Radio Club'},
-        {href:'https://github.com/g4klx',description:'G4KLX Software'},
-        {href:'https://github.com/johnhays/QSOChat',description:'QSOChat Source'}
+        {href:'https://github.com/parsec/QSOChat-docker',description:'QSOChat-docker Repo'}
 ]; //a JSON encoded list of links
 module.exports.standardfreqs = [ "7.287.5", "5346.5", "3.975" ]; //an array of frequencies to use for preselect buttons;
 
